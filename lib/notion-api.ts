cfc64cf28c367f9c1905e450761599bfa128c3cd
kevin.c <kevin345618@outlook.com>
2023-08-21 01:27:21 +0800

    no private page in production

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -2,5 +2,8 @@ import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
   apiBaseUrl: process.env.NOTION_API_BASE_URL,
-  authToken: process.env.NOTION_AUTH_TOKEN
+  authToken:
+    process.env.NODE_ENV === 'development'
+      ? process.env.NOTION_AUTH_TOKEN
+      : null
 })
